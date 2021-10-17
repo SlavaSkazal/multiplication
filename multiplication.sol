4f4c11fdf5d5cacebf1f30cf1e8722a8e7ad6963
SlavaSkazal <slavaskazal@gmail.com>
2021-10-17 21:29:30 +0300

    first commit

---
+++ multiplication.sol
@@ -0,0 +1,19 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract multiplication {
+    
+	uint public number = 1;
+
+	constructor() public {
+		require(tvm.pubkey() != 0, 101);
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	function multiply (uint value) public {
+		require(value > 0 && value < 11, 101);
+        tvm.accept();
+        number *= value;
+	}
+}
